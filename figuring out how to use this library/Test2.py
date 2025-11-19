8d7c461d200483bdcb887124f76c6c57da4a3edb
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-11-19 17:23:39 -0500

    Fix spelling error and finalize intro PoC

--- figuring out how to use this library/Test2.py
+++ figuring out how to use this library/Test2.py
@@ -14,19 +14,20 @@ class Test2(Scene):
         self.play(Write(title))
         self.wait(2)
         ToC = Table(
-            [["Intro to Algorithms, Programming,\n and Compilers", "Variable and Data Types"],
+            [["Intro to Algorithms, Programming,\nand Compilers", "Variable and Data Types"],
             ["Expressions and Output", "Assignment Statements and Inputs"],
-            ["Casing and Range of Variables", "Compound Assignment Operators"]],
+            ["Casting and Range of Variables", "Compound Assignment Operators"]],
             include_outer_lines=True,
             element_to_mobject=Text)
-        sect1 = copy.deepcopy(ToC.get_entries()[0])
-        sect1.scale(1/2)
+        sectTitle = copy.deepcopy(ToC.get_entries()[0])
+        sectTitle.scale(1/2)
         ToC.scale(1/2)
-        sect1.move_to(ToC.get_entries()[0])
+        sectTitle.move_to(ToC.get_entries()[0])
         self.play(Transform(title,updatedTitle))
         self.play(DrawBorderThenFill(ToC), run_time=4)
-        self.add(sect1)
+        self.add(sectTitle)
         self.wait(2)
-        self.play(sect1.animate.move_to(ORIGIN).scale(2),FadeOut(ToC))
+        self.play(sectTitle.animate.move_to(ORIGIN).scale(2),FadeOut(ToC))
         self.wait(2)
-        self.play(FadeOut(sect1))
+        self.play(FadeOut(title),sectTitle.animate.move_to(UP*3.2))
+        self.wait(2)
