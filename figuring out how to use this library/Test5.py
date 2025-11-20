e5c342dfce21c91e45a54ab306e378764760da23
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-11-20 15:05:57 -0500

    FINALLLYYYYYYY

--- figuring out how to use this library/Test5.py
+++ figuring out how to use this library/Test5.py
@@ -5,7 +5,6 @@ config.media_width = "75%"
 config.verbosity = "WARNING"
 print(mn.__version__)
 manim --quality=h Test5
-import copy
 class Test5(Scene):
     def construct(self):
         code_string_complete = '''public class MyProgram
@@ -15,27 +14,13 @@ class Test5(Scene):
         System.out.println("Hello, world!");
     }
 }'''
-        code_string_incomplete = '''public class MyProgram
-{
-    public static void main(String[] args)
-    {
-        
-    }
-}'''
-        incomplete = Code(
-            code_string=code_string_incomplete, tab_width=4, paragraph_config=dict(font="Monospace"), language = "java", add_line_numbers=True,
-            formatter_style='github-dark'
-        )
         complete = Code(
             code_string=code_string_complete, tab_width=4, paragraph_config=dict(font="Monospace"), language = "java", add_line_numbers=True,
             formatter_style='github-dark'
         )
-        extra = complete.code_lines[4]
+        extra = complete.code_lines[4].copy()
         complete.width = 11
-        incomplete.width = 11
-        #incomplete.height = complete.height
-        self.play(Write(complete))
-        self.remove(extra)
+        self.play(Write(complete),Unwrite(complete.code_lines[4]))
         self.wait(2)
         self.play(Write(extra))
         self.wait(2)
