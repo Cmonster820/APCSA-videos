980ef31540cda9bae32dec0769019f761a780be4
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-11-20 08:12:08 -0500

    make random example 1

--- Java Example Programs/Random/Example1.java
+++ Java Example Programs/Random/Example1.java
@@ -1,1 +1,28 @@
-//random example
+//random example (sorting String)
+public class Example1
+{
+    public static String[] sortStrs(String[] toSort)
+    {
+        boolean sorted = false;
+        while (!sorted) {
+            for (int i = 0; i < toSort.length-1; i++) {
+                if (toSort[i].compareTo(toSort[i+1])>0) {
+                    String temp = toSort[i];
+                    toSort[i] = toSort[i+1];
+                    toSort[i+1] = temp;
+                }
+            }
+            for (int i = 0; i < toSort.length-1; i++) {
+                if (toSort[i].compareTo(toSort[i+1])>0)
+                    break;
+                if (i == toSort.length-2)
+                    sorted = true;
+            }
+        }
+        for (int i = 0; i < toSort.length-1; i++) {
+            if (toSort[i]==toSort[i+1])
+                toSort[i] = "";
+        }
+        return toSort 
+    }
+}
