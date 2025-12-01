83094e5f19cd2b9ba25eab78aaef1e126ebdbd9f
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-12-01 13:43:00 -0500

    not done need to save work

--- Math stuff (need to figure out stuff morer)/That one thing.py
+++ Math stuff (need to figure out stuff morer)/That one thing.py
@@ -5,6 +5,8 @@ config.media_width = "75%"
 config.verbosity = "WARNING"
 print(mn.__version__)
 manim --quality=h big10FTC
+%%manim -qm Big10FTC
+
 class Big10FTC(Scene):
     def construct(self):
         banner = ManimBanner()
@@ -19,6 +21,8 @@ class Big10FTC(Scene):
             x_length = 12,
             y_length = 5
         )
+        trap1 = Polygon(np1.coords_to_point(-4,0),np1.coords_to_point(-2,2),np1.coords_to_point(-2,0), color=YELLOW, fill_opacity=0.5)
+        trap2 = Polygon(np1.coords_to_point(-2,0),np1.coords_to_point(-2,2),np1.coords_to_point(0,2),np1.coords_to_point(0,0), color=YELLOW, fill_opacity=0.5)
         xvals = [-4,-2,0,2,6]
         yvals = [0,2,2,-2,2]
         self.play(Create(np1, run_time=3, lag_ratio=0.1))
@@ -42,4 +46,20 @@ class Big10FTC(Scene):
         info.to_corner(UR)
         self.play(Write(info))
         self.wait(2)
-        question = MathTex(r"")
+        question = MathTex(r"find~g(-2)=",r"3")
+        question.next_to(np1,DOWN)
+        self.play(Write(question[0]))
+        explanation1p1 = MathTex(r"g(x)=x+\int_{0}^{x}\!f(t)~dt")
+        explanation1p1.next_to(question,DOWN)
+        explanation1p2 = MathTex(r"g(-2)=(-2)+",r"\int_{0}^{-2}\!f(t)~dt")
+        explanation1p2.move_to(explanation1p1)
+        self.wait(2)
+        self.play(Write(explanation1p1))
+        self.wait(1)
+        self.play(Transform(explanation1p1,explanation1p2))
+        self.add(explanation1p2)
+        self.remove(explanation1p1)
+        self.wait(1)
+        box1 = SurroundingRectangle(explanation1p2[1],buff=0.1)
+        self.play(Create(box1))
+        self.wait(2)
