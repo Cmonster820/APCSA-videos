b5849acb0e76f03f263ae226f1b447bed7e82724
Cooper Howard <72608260+Cmonster820@users.noreply.github.com>
2026-03-13 13:37:32 -0400

    start adding comments

--- ACT stuff/Second_Thing.py
+++ ACT stuff/Second_Thing.py
@@ -10,25 +10,25 @@
 
 class ACTThing2(Scene):
     def construct(self):
-        banner = ManimBanner()
-        self.play(banner.create())
-        self.play(banner.expand())
-        self.wait(0.5)
-        self.play(Unwrite(banner))
+        banner = ManimBanner()      #Make Manim logo mobject
+        self.play(banner.create())  #draw Manim logo mobject
+        self.play(banner.expand())  #expand                 
+        self.wait(0.5)              #wait                   
+        self.play(Unwrite(banner))  #go away                
         question = Tex(r"Sitting in the comfort of {{our}} living room, {{\underline{Bad Bunny’s halftime performance that we were watching}}} \newline was interesting and exciting.",
-                      tex_environment="flushleft")
-        question.to_corner(UL)
+                      tex_environment="flushleft") #^define question mobject
+        question.to_corner(UL) #put it in the corner
         answers = Tex(r"\indent a) Bad Bunny’s halftime performance watched \newline",
                       r"\indent b) there was Bad Bunny’s halftime performance that we \newline \indent \medskip watched \newline",
                       r"\indent c) watching Bad Bunny’s halftime performance that \newline",
                       r"\indent d) we watched Bad Bunny’s halftime performance that",
-                      tex_environment="flushleft")
-        answers.next_to(question,[0,-2,0])
-        answers.to_edge(LEFT,buff=0.75)
-        group = VGroup(question,answers)
-        self.play(Write(group))
-        self.wait(2)
-        new_q = question.copy()
+                      tex_environment="flushleft") #define the answer mobject
+        answers.next_to(question,[0,-2,0])  #put the answers in the right place
+        answers.to_edge(LEFT,buff=0.75)     #↲
+        group = VGroup(question,answers)    #group them together
+        self.play(Write(group))         #write stuff to screen
+        self.wait(2)                #wait
+        new_q = question.copy()     #I don't want to subject you to understanding the hellish fuckery going on here
         new_q.font_size = question.font_size/2
         new_q.to_corner(UL)
         new_a = Tex(r"\indent a) Bad Bunny’s halftime performance watched \newline",
@@ -42,40 +42,40 @@ class ACTThing2(Scene):
         self.play(Transform(group,new_group))
         box1 = SurroundingRectangle(question[3])
         box2 = SurroundingRectangle(question[1])
-        exp2 = Tex(r'"our" states that we are the subject',tex_environment="flushleft")
-        exp1 = Tex(r'''Structure implies that the performance is the subject, but it's "we."''',tex_environment="flushleft")
-        exp1.font_size = exp1.font_size*0.75
-        exp2.font_size = exp1.font_size
-        exp2.to_corner(DR)
-        exp2.to_edge(RIGHT, buff = 3)
-        exp1.next_to(exp2,UP, aligned_edge = LEFT)
-        arr1 = Arrow(start = box1, end = exp1, color = BLUE)
-        arr2 = Arrow(start = box2, end = exp2, color = BLUE)
-        self.play(Create(box2), Create(box1), Write(exp1), Write(exp2), Create(arr1), Create(arr2))
+        exp2 = Tex(r'"our" states that we are the subject',tex_environment="flushleft")                                     #define explanations
+        exp1 = Tex(r'''Structure implies that the performance is the subject, but it's "we."''',tex_environment="flushleft")#↲
+        exp1.font_size = exp1.font_size*0.75#set font size
+        exp2.font_size = exp1.font_size     #↲
+        exp2.to_corner(DR)                          #move to corner
+        exp2.to_edge(RIGHT, buff = 3)               #↲
+        exp1.next_to(exp2,UP, aligned_edge = LEFT)  #↲
+        arr1 = Arrow(start = box1, end = exp1, color = BLUE)    #define arrows
+        arr2 = Arrow(start = box2, end = exp2, color = BLUE)    #↲
+        self.play(Create(box2), Create(box1), Write(exp1), Write(exp2), Create(arr1), Create(arr2)) #draw all that to the screen
         self.wait(2)
-        expGroup = VGroup(exp1,exp2)
-        expGroup.font_size = exp1.font_size
-        explanation = Tex(r"Subject must be first word after comma.")
-        explanation.to_corner(DR)
-        self.play(Uncreate(arr1), Uncreate(arr2),Transform(expGroup,explanation))
-        self.add(explanation)
-        self.remove(expGroup)
-        self.wait(2)
-        self.play(Uncreate(box1), Uncreate(box2))
-        self.wait(2)
+        expGroup = VGroup(exp1,exp2)                                                #####################################
+        expGroup.font_size = exp1.font_size                                         #Man I wish I knew                  #
+        explanation = Tex(r"Subject must be first word after comma.")               #                                   #
+        explanation.to_corner(DR)                                                   #                                   #
+        self.play(Uncreate(arr1), Uncreate(arr2),Transform(expGroup,explanation))   #                                   #
+        self.add(explanation)                                                       #                                   #
+        self.remove(expGroup)                                                       #                                   #
+        self.wait(2)                                                                #                                   #
+        self.play(Uncreate(box1), Uncreate(box2))                                   #                                   #
+        self.wait(2)                                                                #####################################
         lines = []
-        for line in answers:
-            lines.append(Line(
-            start=line.get_left(),
-            end=line.get_right(),
-            color=RED))
-        del lines[3]
+        for line in answers:            #see previous
+            lines.append(Line(          #
+            start=line.get_left(),      #
+            end=line.get_right(),       #
+            color=RED))                 #
+        del lines[3]    #delete strikethrough for right answer
         for line in lines:
-            self.play(Create(line),run_time=0.5)
+            self.play(Create(line),run_time=0.5)    #actually strike through
         box = SurroundingRectangle(answers[3])
         self.play(Create(box))
         self.wait(2)
         for line in lines:
-            self.play(Uncreate(line),run_time = 0.25)
-        self.play(Uncreate(box), Unwrite(group), Unwrite(explanation))
+            self.play(Uncreate(line),run_time = 0.25)                   #remove all
+        self.play(Uncreate(box), Unwrite(group), Unwrite(explanation))  #↲
         self.wait(2)
--- ACT stuff/Thing.py
+++ ACT stuff/Thing.py
@@ -10,53 +10,53 @@
 
 class ACTThing(Scene):
     def construct(self):
-        banner = ManimBanner()
-        self.play(banner.create())
-        self.play(banner.expand())
-        self.wait(0.5)
-        self.play(Unwrite(banner))
+        banner = ManimBanner()      #Make Manim logo mobject
+        self.play(banner.create())  #draw Manim logo mobject
+        self.play(banner.expand())  #expand                 
+        self.wait(0.5)              #wait                   
+        self.play(Unwrite(banner))  #go away                
         question = Tex(r"Adele {{has}} become popular for her unique voice, and \newline she {{\underline{had published}}} four studio albums.",
-                      tex_environment="flushleft")
+                      tex_environment="flushleft") #define the question mobject
         question.to_corner(UL)
         answers = Tex(r"\indent a) NO CHANGE \newline", r"\indent b) has published \newline",r"\indent c) published \newline",r"\indent d) have published",
-                     tex_environment="flushleft")
-        answers.next_to(question,[0,-2,0])
-        answers.to_edge(LEFT,buff=0.75)
+                     tex_environment="flushleft") #define the answers mobject
+        answers.next_to(question,[0,-2,0]) #move the answers next to the question 
+        answers.to_edge(LEFT,buff=0.75) #set the answers to the side of the screen (I was unaware of the `aligned_edge` kwarg at the time)
         group = VGroup(question,answers)
         self.play(Write(group))
         self.wait(2)
-        box1 = SurroundingRectangle(question[1])
-        box2 = SurroundingRectangle(question[3])
-        exp1 = Tex(r"Present perfect")
-        exp2 = Tex(r"Past perfect")
-        exp2.move_to(RIGHT)
-        exp1.next_to(exp2,DOWN)
-        arr1 = Arrow(start = box1, end = exp1, color = BLUE)
-        arr2 = Arrow(start = box2, end = exp2, color = BLUE)
-        self.play(Create(box2), Create(box1), Write(exp1), Write(exp2), Create(arr1), Create(arr2))
+        box1 = SurroundingRectangle(question[1]) #create the box around "has"
+        box2 = SurroundingRectangle(question[3]) #create the box around the underlined stuff
+        exp1 = Tex(r"Present perfect")  #self explanatory
+        exp2 = Tex(r"Past perfect")     #↲
+        exp2.move_to(RIGHT)             #↲
+        exp1.next_to(exp2,DOWN)         #↲
+        arr1 = Arrow(start = box1, end = exp1, color = BLUE) #define first arrow
+        arr2 = Arrow(start = box2, end = exp2, color = BLUE) #↲ second
+        self.play(Create(box2), Create(box1), Write(exp1), Write(exp2), Create(arr1), Create(arr2)) #draw the stuff on the screen
         self.wait(2)
         expGroup = VGroup(exp1,exp2)
         explanation = Tex(r"Must stay the same")
         explanation.move_to(expGroup)
         self.play(Uncreate(arr1), Uncreate(arr2),Transform(expGroup,explanation))
-        self.add(explanation)
-        self.remove(expGroup)
+        self.add(explanation)   #swap out the `expGroup` mobject with the `explanation` mobject 
+        self.remove(expGroup)   #↲
         self.wait(2)
-        self.play(Uncreate(box1), Uncreate(box2))
+        self.play(Uncreate(box1), Uncreate(box2)) #go away boxes
         self.wait(2)
         lines = []
-        for line in answers:
+        for line in answers: #insert the `line` mobjects into the `lines` list
             lines.append(Line(
             start=line.get_left(),
             end=line.get_right(),
             color=RED))
-        del lines[1]
-        for line in lines:
-            self.play(Create(line))
-        box = SurroundingRectangle(answers[1])
-        self.play(Create(box))
+        del lines[1]    #remove the strikethrough line from the correct answer
+        for line in lines:          #loop through the `line`s in `lines` and draw them to the screen
+            self.play(Create(line)) #↲
+        box = SurroundingRectangle(answers[1]) #create the box for the right answer
+        self.play(Create(box))  #draw the aforementioned box to the screen
         self.wait(2)
-        for line in lines:
-            self.play(Uncreate(line))
-        self.play(Uncreate(box), Unwrite(group), Unwrite(explanation))
-        self.wait(2)
+        for line in lines:              #loop through all the `line`s in `lines` and remove them from the screen
+            self.play(Uncreate(line))   #↲
+        self.play(Uncreate(box), Unwrite(group), Unwrite(explanation))  #fancily remove everything left
+        self.wait(2)    #wait 2 seconds
