1bc821114f75309f17990782247a698c255f56d8
Cooper Howard <72608260+Cmonster820@users.noreply.github.com>
2026-03-24 08:57:54 -0400

    paste template

---
+++ Pre-real video stuff/Recursions.py
@@ -0,0 +1,9 @@
+%%manim -qm Recursions
+class Recursions(Scene):
+    def construct(self):
+        banner = ManimBanner()
+        self.play(banner.create())
+        self.play(banner.expand())
+        self.wait()
+        self.play(Unwrite(banner))
+        self.wait(2)
