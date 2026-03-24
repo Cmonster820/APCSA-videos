9f1ac2744c95ba6277c9792145ea855ebd66f3c7
Cooper Howard <72608260+Cmonster820@users.noreply.github.com>
2026-03-24 09:44:46 -0400

    should be done just neeed to render  in qh

--- Pre-real video stuff/Recursions.py
+++ Pre-real video stuff/Recursions.py
@@ -29,8 +29,43 @@ class Recursions(Scene):
         formatter_style='monokai')
         self.play(Write(code_first))
         self.wait(2)
-        code_second.height=code_first.height*(3/4)
+        code_second.width=code_first.width/2
         code_second.to_edge(LEFT)
         self.play(Transform(code_first,code_second))
         self.add(code_second)
         self.remove(code_first)
+        code_third = Code(code_string='''...
+        factorial(5);
+...''',
+                          language="java",
+                          formatter_style="monokai")
+        code_third.next_to(code_second,RIGHT)
+        self.play(Write(code_third))
+        self.wait(2)
+        self.play(code_third.animate.to_corner(UR))
+        squares = []
+        answers = [120, 24, 6, 2, 1]
+        for i in range(5,0,-1):
+            code = Code(code_string=f"factorial({i});",language = "java",formatter_style = "monokai")
+            table = MobjectTable( [[code]],
+                           include_outer_lines=True)
+            table.to_edge(RIGHT)
+            squares.append(table)
+        for table in squares:
+            box = SurroundingRectangle(code_second)
+            self.play(Create(box),run_time=0.5)
+            self.play(Write(table),run_time=0.5)
+            self.play(Uncreate(box),run_time=0.5)
+        for i in range(5,-1,-1):
+            newTable = Table([[str(answers[i])]], include_outer_lines=True)
+            self.play(Transform(squares[i],newTable))
+            self.add(newTable)
+            self.remove(squares[i])
+            thing = MathTex(answers[i])
+            thing.move_to(newTable)
+            self.add(thing)
+            self.play(Unwrite(newTable),thing.animate.move_to(code_second))
+            self.play(Unwrite(thing))
+        self.wait(2)
+        self.play(Unwrite(code_second),Unwrite(code_third),Unwrite(Title))
+        self.wait(2)
