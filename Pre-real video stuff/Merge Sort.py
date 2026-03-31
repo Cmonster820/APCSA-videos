895fc01c7e3171e7845adb711163f04609fc72b4
Cooper Howard <72608260+Cmonster820@users.noreply.github.com>
2026-03-31 13:37:13 -0400

    almost done I think

--- Pre-real video stuff/Merge Sort.py
+++ Pre-real video stuff/Merge Sort.py
@@ -13,8 +13,7 @@ class MergeSort(Scene):
         self.wait(1)
         self.play(title.animate.to_edge(UP))
         self.wait(1)
-        numtable = [i for i in range(1,9)]
-        random.shuffle(numtable)
+        numtable = [3,5,4,8,2,6,7,1]
         table = []
         for num in numtable:
             item = Tex(num)
@@ -34,9 +33,16 @@ class MergeSort(Scene):
         divider1 = Line(start = table[len(table)//4].get_corner(UL), end = table[len(table)//4].get_corner(DL), color = RED)
         divider2 = Line(start = table[len(table)*3//4].get_corner(UL), end = table[len(table)*3//4].get_corner(DL), color = RED)
         self.play(Create(divider1),Create(divider2))
-        subgroup1 = VGroup(*[item for item in table[:len(table)//4]])
-        subgroup2 = VGroup(*[item for item in table[len(table)//4:len(table)//2]])
-        subgroup3 = VGroup(*[item for item in table[len(table)//2:len(table)*3//4]])
-        subgroup4 = VGroup(*[item for item in table[len(table)*3//4:]])
-        self.play(subgroup1.animate.shift([-0.5,0,0]),subgroup4.animate.shift([0.5,0,0]),divider1.animate.shift([-0.25,0,0]),divider2.animate.shift([0.25,0,0]))
+        subgroup3 = VGroup(*[item for item in table[:len(table)//4]])
+        subgroup4 = VGroup(*[item for item in table[len(table)//4:len(table)//2]])
+        subgroup5 = VGroup(*[item for item in table[len(table)//2:len(table)*3//4]])
+        subgroup6 = VGroup(*[item for item in table[len(table)*3//4:]])
+        self.play(subgroup3.animate.shift([-0.5,0,0]),subgroup6.animate.shift([0.5,0,0]),divider1.animate.shift([-0.25,0,0]),divider2.animate.shift([0.25,0,0]),Uncreate(divider1),Uncreate(divider2))
+        dividerslist = [Line(start = item.get_corner(UL), end = item.get_corner(DL), color = RED) for item in table]
+        del dividerslist[0::2]
+        dividers = VGroup(*dividerslist)
+        self.play(*[Create(item) for item in dividerslist])
+        self.play(table[0].animate.shift([-1,0,0]),table[1].animate.shift([-0.5,0,0]),table[2].animate.shift([-0.5,0,0]),table[-3].animate.shift([0.5,0,0]),table[-2].animate.shift([0.5,0,0]),table[-1].animate.shift([1,0,0]), *[Uncreate(item) for item in dividerslist])
         self.wait(2)
+        boxes = [SurroundingRectangle(item, buff=0.1) for item in [subgroup3,subgroup4,subgroup5,subgroup6]]
+        self.play(*[Create(item) for item in boxes])
