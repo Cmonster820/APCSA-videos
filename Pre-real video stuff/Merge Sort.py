f4a6ce7135f7dd8fef09aff118f3c6e0c6ff678b
Cooper Howard <72608260+Cmonster820@users.noreply.github.com>
2026-03-31 09:38:55 -0400

    gtg rn

--- Pre-real video stuff/Merge Sort.py
+++ Pre-real video stuff/Merge Sort.py
@@ -1,4 +1,5 @@
 %%manim -qm MergeSort
+import random
 class MergeSort(Scene):
     def construct(self):
         banner = ManimBanner()
@@ -12,3 +13,17 @@ class MergeSort(Scene):
         self.wait(1)
         self.play(title.animate.to_edge(UP))
         self.wait(1)
+        numtable = [i for i in range(1,9)]
+        random.shuffle(numtable)
+        table = []
+        for num in numtable:
+            item = Tex(num)
+            square = Square(item.height*1.5)
+            group = VGroup(item,square)
+            table.append(group)
+        table[0].move_to([-len(table)//2*table[0].width,0,0])
+        for i in range(1,len(table)):
+            table[i].next_to(table[i-1],buff=0)
+        for item in table:
+            self.play(Write(item),run_time=0.5)
+        
